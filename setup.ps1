f42dc78c93ef688b43335b9281f313738b6dbbcf
gekal <liu.hongying@hotmail.com>
2023-05-01 14:14:31 +0900

    enable php for apache

--- setup.ps1
+++ setup.ps1
@@ -39,24 +39,24 @@ If (!(test-path ".\Apache24")) {
 Write-Host "----------------------------------------"
 
 $version = "8.1.18"
-$url = "https://www.apachehaus.com/downloads/httpd-${version}-o111s-x64-vs17.zip"
+$url = "https://windows.php.net/downloads/releases/php-${version}-Win32-vs16-x64.zip"
 
 Write-Host "apache version: ${version}"
-Write-Host "url: https://windows.php.net/downloads/releases/php-${version}-nts-Win32-vs16-x64.zip"
+Write-Host "url: https://windows.php.net/downloads/releases/php-${version}-Win32-vs16-x64.zip"
 
 If (!(test-path ".\php81")) {
     Write-Host "PHPをダウンロードする。"
-    Write-Host "https://windows.php.net/downloads/releases/php-${version}-nts-Win32-vs16-x64.zip"
+    Write-Host "https://windows.php.net/downloads/releases/php-${version}-Win32-vs16-x64.zip"
     $client = new-object System.Net.WebClient
     $client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-    $client.DownloadFile("https://windows.php.net/downloads/releases/php-${version}-nts-Win32-vs16-x64.zip", ".\php-${version}-nts-Win32-vs16-x64.zip")
+    $client.DownloadFile("https://windows.php.net/downloads/releases/php-${version}-Win32-vs16-x64.zip", ".\php-${version}-Win32-vs16-x64.zip")
 
     Write-Host "ZIPファイルを解凍して、削除する。"
-    Write-Host ".\php-${version}-nts-Win32-vs16-x64 >>>> ."
-    Expand-Archive -Path ".\php-${version}-nts-Win32-vs16-x64.zip" -DestinationPath ".\php-${version}-nts-Win32-vs16-x64" -Force
-    Remove-Item ".\php-${version}-nts-Win32-vs16-x64.zip"
+    Write-Host ".\php-${version}-Win32-vs16-x64 >>>> ."
+    Expand-Archive -Path ".\php-${version}-Win32-vs16-x64.zip" -DestinationPath ".\php-${version}-Win32-vs16-x64" -Force
+    Remove-Item ".\php-${version}-Win32-vs16-x64.zip"
 
-    Rename-Item -Path "php-${version}-nts-Win32-vs16-x64" -NewName "php81"
+    Rename-Item -Path "php-${version}-Win32-vs16-x64" -NewName "php81"
 }
 
 Write-Host "環境変数を設定する"
@@ -69,6 +69,7 @@ Write-Host "----------------------------------------"
 
 Copy-Item "configs\mysql\my.ini" -Destination "mysql80"
 Copy-Item "configs\apache\httpd.conf" -Destination "Apache24\conf"
+Copy-Item "configs\php\php.ini" -Destination "php81"
 
 Write-Host "何かキーを押してください。" -NoNewLine
 [Console]::ReadKey() | Out-Null
